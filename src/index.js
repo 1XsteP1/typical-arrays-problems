ab13dd1a5011cfad74a3e70246a96b902a6973a1
1XsteP1 <forroyalee2@gmail.com>
2020-03-12 18:55:14 +0200

    init: start t.a.p.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+   return 0
+  } else {
+   return Math.min(...array)
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+   return 0
+  } else {
+   return Math.max(...array)
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+ exports.avg = function avg (array) {
+  if (array == undefined || array.length == 0) {
+   return 0
+  }
+
+  let average = 0;
+
+   let comp = (a,b) => {
+   if (a > b) return 1
+   if (a == b) return 0
+   if (a < b) return -1
+  }
+
+   array.sort(comp)
+
+   for (let i in array) {
+   average += array[i]
+  }
+
+   return average / array.length
 }
